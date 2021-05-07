48c04d4d40c8e1e47fdba4b1b52914d079a89d21
Tianhao Zhou <tianhaoz@umich.edu>
2021-05-07 00:35:37 -0700

    chore: add gh act for deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://wowlink.github.io/wow-docs',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'wowlink',
+  projectName: 'wow-docs',
   themeConfig: {
     navbar: {
       title: 'My Site',
